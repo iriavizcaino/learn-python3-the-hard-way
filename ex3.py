c5fee9fd9db3938c03826fa47db593cdb982d6e1
pabsan-0 <psantana@catec.aero>
2024-03-04 13:23:26 +0100

    add comments

--- ex3.py
+++ ex3.py
@@ -1,5 +1,6 @@
 print("I will now count my chickens:")
 
+# Realizar cuentas
 print("Hens", 25+30/6)
 print("Roosters", 100-25*3%4)
 
@@ -9,6 +10,7 @@ print(3+2+1-5+4%2-1/4+6)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
+#Condiciones
 print(3+2<5-7)
 
 print("What is 3+2?", 3+2)
