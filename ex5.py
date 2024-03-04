995a7903a5ad9c0062c620c96630e07b628193ae
pabsan-0 <psantana@catec.aero>
2024-03-04 13:51:33 +0100

    round decimals

--- ex5.py
+++ ex5.py
@@ -11,9 +11,9 @@ pound2kg = 1/2.2046
 
 print(f"Let's talk about {name}")
 print(f"He's {height} inches tall")
-print(f"He's {height*inch2cm} cm tall")
+print(f"He's {round(height*inch2cm,2)} cm tall")
 print(f"He's {weight} pounds heavy")
-print(f"He's {weight*pound2kg} kg heavy")
+print(f"He's {round(weight*pound2kg,2)} kg heavy")
 print("Actually that's not too heavy")
 print(f"Heś got {eyes} eyes and {hair} hair")
 print(f"His teeth are usually {teeth} depending on the coffee")
