54e828c3ea442173a19ffd62e281eab0973c0c5e
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-07 09:05:42 +0100

    Add comments

--- ex15.py
+++ ex15.py
@@ -1,13 +1,18 @@
+# Use argv to get a filename
 from sys import argv
 
 script, filename = argv
+
+# Open the file
 txt = open(filename)
 
+# Print a little message
 print(f"Here's your file {filename}")
-print(txt.read())
+print(txt.read())   # Print the content of txt
 
+# Another way to do it
 print("Type the filename again:")
-file_again = input ("> ")
+file_again = input ("> ")   
 
 txt_again = open(file_again)
 
