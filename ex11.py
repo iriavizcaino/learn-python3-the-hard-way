477a03211c4ea919097827d1c8f75e925be98467
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-07 08:13:22 +0100

    Test command input

--- ex11.py
+++ ex11.py
@@ -6,3 +6,14 @@ print("How much do you weigh?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Text input on the next line
+print("How old are you?")
+age1 = input()
+
+# More compact
+age2 = input("How old are you?")
+
+# Conversion
+age3 = int(input("How old are you? (without decimals)"))
+age4 = float(input("How old are you? (with decimals)"))
