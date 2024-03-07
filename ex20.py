8f0572c7fd85e9ea5eb0b83e7adce99932a317f1
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-07 13:16:16 +0100

    Add comments

--- ex20.py
+++ ex20.py
@@ -3,19 +3,19 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-    print(f.read())
+    print(f.read()) # Prints text in f
 
 def rewind(f):
     f.seek(0)
 
 def print_a_line(line_count, f):
-    print(line_count, f.readline())
+    print(line_count, f.readline())  #Prints the line number line_count
 
-current_file = open(input_file)
+current_file = open(input_file) # Open the file input_file
 
 print("First let's print the whole file: \n")
 
-print_all(current_file)
+print_all(current_file) # Calls function print_all 
 
 print("Now let's rewind, kind of like a tape")
 
