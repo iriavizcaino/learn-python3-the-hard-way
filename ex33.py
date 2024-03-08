5244fa7234337a9422ae9ce4163d9d24fb2db949
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-08 12:20:28 +0100

    Convert while-loop to a function

--- ex33.py
+++ ex33.py
@@ -1,17 +1,21 @@
-i = 0
-numbers = []
+def while_loop(top):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+    while i < top:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i = i+1
+        i = i+1
 
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-print("The numbers: ")
+    print("The numbers: ")
 
-for num in numbers:
-    print(num)
-    
+    for num in numbers:
+        print(num)
+
+top = 10
+
+while_loop(top)
