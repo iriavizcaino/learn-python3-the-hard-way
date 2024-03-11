b1ef6f14d28332441d42f2935ed16ce2a571be39
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-11 09:11:43 +0100

    Add variables

--- ex40.py
+++ ex40.py
@@ -7,12 +7,16 @@ class Song(object):
         for line in self.lyrics:
             print(line)
 
-happy_bday = Song(["Happy birthday to you",
-                   "I don't want to get sued",
-                   "So I'll stop right there"])
+bday_lyrics = ["Happy birthday to you",
+            "I don't want to get sued",
+            "So I'll stop right there"]
 
-bulls_on_parade = Song(["They rally around tha family",
-                        "With pockets full of shells"])
+bulls_lyrics = ["They rally around tha family",
+                "With pockets full of shells"]
+
+happy_bday = Song(bday_lyrics)
+
+bulls_on_parade = Song(bulls_lyrics)
 
 happy_bday.sing_me_a_song()
 
