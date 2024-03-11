083a395c9932a60279904954b4f33dca73a6cd84
iriavizcaino <ivizcaino.ext@catec.aero>
2024-03-11 14:08:39 +0100

    Composition

--- ex44.py
+++ ex44.py
@@ -9,24 +9,37 @@ class Parent(object):
     def altered(self):
         print("PARENT altered()")
 
-class Child(Parent):
+class Other(object):
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def override(self):
+        print("OTHER override()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
     
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
     def override(self):
         print("CHILD override()")
 
     def altered(self):
-        print("CHILD, BEFORE PARENT altered()")
-        super(Child, self).altered()
-        print("CHILD, AFTER PARENT altered()")
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
 
-dad = Parent()
+
+
 son = Child()
 
-dad.implicit()
 son.implicit()
-
-dad.override()
 son.override()
-
-dad.altered()
 son.altered()
